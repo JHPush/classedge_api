4b13267711494998cfcfcc44bdb28ca8f82b2bee
PoorHotsix <majch98@gmail.com>
2025-03-10 16:51:44 +0900

    Project initialze

---
+++ src/main/java/com/learnova/classedge/ClassedgeApplication.java
@@ -0,0 +1,13 @@
+package com.learnova.classedge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassedgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassedgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learnova/classedge/ClassedgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnova.classedge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassedgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

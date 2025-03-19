39757430fcff57c5807e94176388704f961ccde6
PoorHotsix <majch98@gmail.com>
2025-03-19 10:45:11 +0900

    Update MemberSignUpController /api/v1/signup

--- src/main/java/com/learnova/classedge/controller/MemberSignUpController.java
+++ src/main/java/com/learnova/classedge/controller/MemberSignUpController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/v1/members")
+@RequestMapping("/api/v1")
 public class MemberSignUpController {
 
     private final MemberSignUpService memberService;

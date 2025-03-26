44f4a590bf965e02941346fd9e1a5ba1fe2a763b
JHPush <qnrntmvls@gmail.com>
2025-03-26 11:59:14 +0900

    파일 업로드 + 게시글 작성

--- src/main/java/com/learnova/classedge/controller/PostController.java
+++ src/main/java/com/learnova/classedge/controller/PostController.java
@@ -3,7 +3,6 @@ package com.learnova.classedge.controller;
 import java.util.List;
 import java.util.Map;
 
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -17,7 +16,9 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.learnova.classedge.domain.MemberRole;
 import com.learnova.classedge.dto.FileItemDto;
@@ -39,49 +40,59 @@ import lombok.extern.slf4j.Slf4j;
 public class PostController {
 
     private final PostService postService;
-    
+
     private final FileItemService fileItemService;
 
-  
     // 게시글 목록조회 - 카테고리 별 http://localhost:8080/api/v1/posts?limit=5
     @GetMapping("/posts")
-    public ResponseEntity<Map<String, Object>> getPostBoard(            
+    public ResponseEntity<Map<String, Object>> getPostBoard(
             @RequestParam(value = "limit", required = false, defaultValue = "5") int limit) {
-            
-            List<PostDto> taskPosts = postService.retrievePostList("TASK", limit);
-            List<PostDto> noticePosts = postService.retrievePostList("NOTICE", limit);          
-     
-            return new ResponseEntity( Map.of("task", taskPosts, "notice", noticePosts), HttpStatus.OK);
+
+        List<PostDto> taskPosts = postService.retrievePostList("TASK", limit);
+        List<PostDto> noticePosts = postService.retrievePostList("NOTICE", limit);
+
+        return new ResponseEntity(Map.of("task", taskPosts, "notice", noticePosts), HttpStatus.OK);
     }
 
-    // 게시글 등록 http://localhost:8080/api/v1/posts/register	
+    // 게시글 등록 http://localhost:8080/api/v1/posts/register
     @PostMapping("/posts/register")
     @PreAuthorize("hasAnyRole('ROLE_PROFESSOR', 'ROLE_ADMIN')")
     public ResponseEntity<Map<String, Long>> postPost(
-            @RequestBody PostDto postDto, 
+            @RequestPart("postDto") PostDto postDto,
+            @RequestPart(value = "files", required = false) List<MultipartFile> files,
             @AuthenticationPrincipal UserDetails userDetails) {
-                
-        MemberDto memberDto = (MemberDto)userDetails;
+
+        MemberDto memberDto = (MemberDto) userDetails;
 
         String nickname = memberDto.getNickname();
         String memberName = memberDto.getMemberName();
-        MemberRole role  = memberDto.getRole();
-   
+        MemberRole role = memberDto.getRole();
+
         postDto.setNickname(nickname);
         log.info("postDto: {}", postDto);
+        log.info("file length: {}", files.size());
         Long id = postService.registerPost(postDto);
+        if (files.size() > 0) {
+            try {
+                List<Long> fileIds = fileItemService.uploadFile(files, id, null);
+
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+
         log.info("id : {}", id);
 
         return new ResponseEntity<>(Map.of("id", id), HttpStatus.CREATED);
 
     }
 
-    // 게시글 상세조회 http://localhost:8080/api/v1/posts/1	
+    // 게시글 상세조회 http://localhost:8080/api/v1/posts/1
     @GetMapping("/posts/{id}")
     public ResponseEntity<PostDto> getPost(@PathVariable("id") Long id) {
-      
+
         PostDto postDto = postService.retrivePost(id);
-        
+
         return new ResponseEntity<>(postDto, HttpStatus.OK);
     }
 
@@ -100,13 +111,14 @@ public class PostController {
         return new ResponseEntity<>("", HttpStatus.OK);
     }
 
-    // 검색과 페이징 처리 http://localhost:8080/api/v1/posts/search?page=1&size=2	&   http://localhost:8080/api/v1/posts/search?page=1&size=10&keyfield=title&keyword=검색테스트
+    // 검색과 페이징 처리 http://localhost:8080/api/v1/posts/search?page=1&size=2 &
+    // http://localhost:8080/api/v1/posts/search?page=1&size=10&keyfield=title&keyword=검색테스트
     @GetMapping("/posts/search")
     public ResponseEntity<PageResponseDto<PostDto>> getPosts(
             @RequestParam(value = "keyfield", required = false) String keyfield,
             @RequestParam(value = "keyword", required = false) String keyword,
             @RequestParam(value = "boardName", required = false) String boardName,
-           
+
             PageRequestDto pageRequestDto) {
 
         log.info("page : {}, size : {}", pageRequestDto.getPage(), pageRequestDto.getSize());
@@ -124,7 +136,7 @@ public class PostController {
         }
 
         // boardName 처리
-        if (boardName !=null) {
+        if (boardName != null) {
             condition.setBoardName(boardName);
         }
 

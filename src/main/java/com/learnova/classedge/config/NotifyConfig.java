9b6e4b25c9f40b9a961753d7195dc5b24d447055
PoorHotsix <majch98@gmail.com>
2025-03-28 20:52:02 +0900

    Update ip address

--- src/main/java/com/learnova/classedge/config/NotifyConfig.java
+++ src/main/java/com/learnova/classedge/config/NotifyConfig.java
@@ -11,9 +11,6 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class NotifyConfig implements WebSocketMessageBrokerConfigurer{
 
-    @Value("${global.front_react}")
-    private String front;
-
     // /app prefix -> @messagemapping 컨트롤러 처리후 /topic prefix를 통해 브로커에게 전달 -> STOMP MESSAGE 메소드로 구독자들에게 response
     // /topic prefix -> 컨트롤러 안거치고 브로커에게 직접 접근
     @Override
@@ -25,7 +22,7 @@ public class NotifyConfig implements WebSocketMessageBrokerConfigurer{
     // /ws 경로로 클라와 웹소켓으로 연결
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("http://" + front).withSockJS();
+        registry.addEndpoint("/api/v1/ws").setAllowedOrigins("http://192.168.248.133:30080").withSockJS();
 
     }
 

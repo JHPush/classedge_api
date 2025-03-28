28b87cc8d9e4f69d662515045d06e4d8b79ea144
PoorHotsix <majch98@gmail.com>
2025-03-28 17:18:15 +0900

    update docker-compose

--- src/main/java/com/learnova/classedge/config/NotifyConfig.java
+++ src/main/java/com/learnova/classedge/config/NotifyConfig.java
@@ -1,5 +1,6 @@
 package com.learnova.classedge.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -9,6 +10,9 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class NotifyConfig implements WebSocketMessageBrokerConfigurer{
+
+    @Value("${global.front_react}")
+    private String front;
 
     // /app prefix -> @messagemapping 컨트롤러 처리후 /topic prefix를 통해 브로커에게 전달 -> STOMP MESSAGE 메소드로 구독자들에게 response
     // /topic prefix -> 컨트롤러 안거치고 브로커에게 직접 접근
@@ -21,7 +25,7 @@ public class NotifyConfig implements WebSocketMessageBrokerConfigurer{
     // /ws 경로로 클라와 웹소켓으로 연결
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:3000").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOrigins("http://" + front).withSockJS();
 
     }
 
